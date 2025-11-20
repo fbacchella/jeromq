0f67e5fd05eee33e67c4b5f33dea566d234c6b00
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2025-11-20 12:17:38 +0100

    Switching to junit 5

--- jeromq-core/src/test/java/zmq/io/net/tcp/KeepAliveTest.java
+++ jeromq-core/src/test/java/zmq/io/net/tcp/KeepAliveTest.java
@@ -4,30 +4,31 @@ import java.io.IOException;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import jdk.net.ExtendedSocketOptions;
 
-public class KeepAliveTest
-{
-    @Test
-    public void testConsistent() throws IOException
-    {
-        int javaspec = Integer.parseInt(System.getProperty("java.specification.version"));
-        // Test fails for jvm less than 13
-        if (javaspec >= 13) {
-            SocketChannel sc = SocketChannel.open();
-            TcpUtils.tuneTcpKeepalives(sc, 1, 2, 3, 4);
-            Assert.assertEquals(2, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPCOUNT));
-            Assert.assertEquals(3, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPIDLE));
-            Assert.assertEquals(4, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPINTERVAL));
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
 
-            ServerSocketChannel ssc = ServerSocketChannel.open();
-            TcpUtils.tuneTcpKeepalives(ssc, 1, 2, 3, 4);
-            Assert.assertEquals(2, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPCOUNT));
-            Assert.assertEquals(3, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPIDLE));
-            Assert.assertEquals(4, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPINTERVAL));
-        }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KeepAliveTest {
+
+    @EnabledForJreRange(min = JRE.JAVA_13)
+    @Test
+    void testConsistent() throws IOException {
+        SocketChannel sc = SocketChannel.open();
+        TcpUtils.tuneTcpKeepalives(sc, 1, 2, 3, 4);
+
+        assertEquals(2, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPCOUNT));
+        assertEquals(3, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPIDLE));
+        assertEquals(4, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPINTERVAL));
+
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        TcpUtils.tuneTcpKeepalives(ssc, 1, 2, 3, 4);
+
+        assertEquals(2, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPCOUNT));
+        assertEquals(3, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPIDLE));
+        assertEquals(4, (int) sc.socket().getOption(ExtendedSocketOptions.TCP_KEEPINTERVAL));
     }
 }
