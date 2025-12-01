6620e68f7c08d254fe37b40285afbfa1c9d64d29
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2025-12-01 18:59:26 +0100

    Start to provide a correct logging environment for tests that can hides logs when not needed.

---
+++ jeromq-core/src/test/java/org/zeromq/DynamicLog4jFilter.java
@@ -0,0 +1,43 @@
+package org.zeromq;
+
+import java.util.Optional;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Core;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.filter.AbstractFilter;
+
+@Plugin(name = "DynamicFilter", category = Core.CATEGORY_NAME)
+public class DynamicLog4jFilter extends AbstractFilter
+{
+    private final boolean inCircleCI;
+    private final boolean explicitDeactivation;
+
+    public DynamicLog4jFilter() {
+        inCircleCI = Optional.ofNullable(System.getenv("CIRCLECI")).map(Boolean::valueOf).orElse(false);
+        explicitDeactivation = Optional.ofNullable(System.getProperty("maven.logging.skip")).map(Boolean::valueOf).orElse(false);
+    }
+
+    @Override
+    public Result filter(LogEvent event)
+    {
+        if (event.getLoggerName().startsWith("org.junit"))
+        {
+            return Result.ACCEPT;
+        }
+        else if ((inCircleCI || explicitDeactivation) && event.getLevel().isLessSpecificThan(Level.ERROR))
+        {
+            return Result.DENY;
+        } else {
+            return Result.ACCEPT;
+        }
+    }
+
+    @PluginFactory
+    public static DynamicLog4jFilter createFilter()
+    {
+       return new DynamicLog4jFilter();
+    }
+}
