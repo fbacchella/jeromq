f07c0a5c78c18bf447bd565c547a8da80e23255b
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2025-12-03 10:37:28 +0100

    Fixing logging reconfiguration to hides them on junit.

--- jeromq-core/src/test/java/org/zeromq/JunitLoggingReconfigurator.java
+++ jeromq-core/src/test/java/org/zeromq/JunitLoggingReconfigurator.java
@@ -8,15 +8,21 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 /**
- * Logging is configured too early in the junit lifecycle, so we need to configure it here.
+ * Logging is configured too early in the junit lifecycle, so an arbitrary extension is defined.
+ * When JUnit resolves the class, logging is reconfigured.
  */
-public class JunitLoggingConfiguration implements BeforeAllCallback {
-    @Override
-    public void beforeAll(ExtensionContext context)
+public class JunitLoggingReconfigurator implements BeforeAllCallback {
+    static
     {
         LoggerContext lctx = (LoggerContext) LogManager.getContext(false);
         Configuration conf = lctx.getConfiguration();
         AbstractFilterable filterable = conf.getAppender("Console");
         filterable.addFilter(new DynamicLog4jFilter());
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext context)
+    {
+        // Nothing to do, instantiate the extension was enough
     }
 }
